ce609435a7a4d0e54b47724ad0e0b830dd3dc7f5
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-03-02 16:59:40 +0600

    Update README, add Tailwind CSS configuration, and modify layout and button styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import { Button } from '@/components/ui/button';
-import React from 'react';
 
 const HomePage = () => {
+
     return (
         <div>
-            <Button>Click me</Button>
+            <Button variant="outline" >Click Me</Button>
         </div>
     );
 };
